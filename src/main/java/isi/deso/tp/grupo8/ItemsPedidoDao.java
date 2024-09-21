fc4d3e4ade1a4ae5033bfb64439bbe92864fa036
NicoGar2000 <117773324+NicoGar2000@users.noreply.github.com>
2024-09-21 19:08:59 -0300

    Update ItemsPedidoDao.java

--- src/main/java/isi/deso/tp/grupo8/ItemsPedidoDao.java
+++ src/main/java/isi/deso/tp/grupo8/ItemsPedidoDao.java
@@ -7,7 +7,7 @@ package isi.deso.tp.grupo8;
 import java.util.Set;
 
 public interface ItemsPedidoDao {
-
+    Set<ItemPedido> filtrado(char filtro) throws ItemNoEncontradoException;
     Set<ItemPedido> ordenarPorCriterio();
     Set<ItemPedido> busquedaPorRangodePecios(double min, double max) throws ItemNoEncontradoException ;
     Set<ItemPedido> buscarPorRestaurante(Vendedor v) throws ItemNoEncontradoException;
