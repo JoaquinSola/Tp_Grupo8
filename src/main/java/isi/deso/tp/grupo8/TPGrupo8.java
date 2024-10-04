1fe7c1bc249a01e64f8bdfc3ffc5fe21f815e6dd
violetaferrero <95654571+violetaferrero@users.noreply.github.com>
2024-10-04 17:46:26 -0300

    4/10 17:46

--- src/main/java/isi/deso/tp/grupo8/TPGrupo8.java
+++ src/main/java/isi/deso/tp/grupo8/TPGrupo8.java
@@ -115,16 +115,20 @@ public class TPGrupo8 {
         vendedores[2] = v3;
         vendedores[3] = v4;
         //busqueda por nombre o id
-        //buscarVendedor(vendedores,"Marcelo");
+        System.out.println("BUSCAR VENDEDORES ---"); 
         buscarVendedor(vendedores, "Florencia");
+        //buscarVendedor(vendedores, "Horacio");
+        //buscarVendedor(vendedores, "Rodrigo");
+        buscarVendedor(vendedores, "Marcelo");
         System.out.println("\n");
+        System.out.println("ELIMINAMOS UN VENDEDOR ---"); 
         vendedores = eliminarVendedor(vendedores, buscarVendedor(vendedores,"Marcelo"));
         //---------------------------------------------CLIENTE----------------------------------------------  
         //coordenadas cliente
         Coordenada coorC1 = new Coordenada (45.3f,99.3f);
         Coordenada coorC2 = new Coordenada (18.9f,78.6f);
         Coordenada coorC3 = new Coordenada (65.36f,91.5f);
-        //clientes
+        //clientes 
         Cliente c1 = new Cliente ("c00001", "20-30495842-7", "jAlvarez@gmail.com","San Martin 1523", coorC1);
         Cliente c2 = new Cliente ("c00002", "27-15378964-3", "juanPerez25@outlook.es","9 de Julio 2876", coorC2);
         Cliente c3 = new Cliente ("c00003", "20-27894561-9", "lopezLopezCarlos@hotmail.com"," Gobernador Crespo 4201", coorC3);
@@ -135,20 +139,27 @@ public class TPGrupo8 {
         clientes[2] = c3;
         System.out.println("\n");
         //buscar clientes
+        System.out.println("BUSCAR CLIENTE ---");
         buscarCliente(clientes, "c00003");
-        System.out.println("\n");
+         buscarCliente(clientes, "c00001");
+        //System.out.println("\n");
         //eliminar cliente por id
+        System.out.println("\n");
+        System.out.println("ELIMINAMOS UN CLIENTE ---"); 
         clientes= eliminarCliente(clientes,buscarCliente(clientes,"c00001"));
         //imprimir distancia en km
+        System.out.println("DISTANCIA EN KILOMETROS ENTRE VENDEDOR Y CLIENTE ---"); 
         System.out.println("La distancia es de: "+v2.distancia(c2)+"Km");
 
         //INSTANCIA 2
+        //System.out.println("PLATOS ---"); 
         Plato plato1 = new Plato(15,false,false,"Bife",15);
         Plato plato2 = new Plato(15,true,true,"Lechuga",69);
         Plato plato3 = new Plato(23,true,false,"Pizza",12);
         Plato plato4 = new Plato(18,false,true,"Wok de verduras",225.5);
         Plato plato5 = new Plato(20,false,false,"Alfajor",10);
         Plato plato6 = new Plato(36,false,false,"Papa Frita",39);
+        //System.out.println("BEBIDAS ---"); 
         Bebida bebida1 = new Bebida(40, 450,"Vodka",25);
         Bebida bebida2 = new Bebida(0, 450,"Manaos",30);
         Bebida bebida3 = new Bebida(0, 500,"Limonada",33);
@@ -171,19 +182,23 @@ public class TPGrupo8 {
         productos4.add(bebida6);
         productos4.add(plato5);
         productos4.add(plato6);
-
+        
+        System.out.println("PESO DEL PEDIDO Y CLASIFICACION ---"); 
         System.out.println("El peso del plato es: "+plato1.peso(15));
         plato1.esComida();
         plato1.esBebida();
         System.out.println("El peso de la bebida es: "+bebida1.peso(15));
         bebida1.esComida();
         bebida1.esBebida();
+        System.out.println(); 
         System.out.println("Comidas asociadas al vendedor "+v2.getNombre()+": "+v2.getComidas()); //IMPRIME LISTA DE COMIDAS QUE VENDE ESE VENDEDOR
         System.out.println("Bebibdas asociadas al vendedor "+v2.getNombre()+": "+v2.getBebidas()); //IMPRIME LISTA DE BEBIDAS QUE VENDE ESE VENDEDOR
-        v2.mostrarProductos();                                                      //IMPRIME LISTA TOTAL QUE VENDE ESE VENDEDOR
-        v2.bebidaSinAlcohol();                                                      //IMPRIME LISTA DE BEBIDAS SIN ALCOHOL QUE VENDE ESE VENDEDOR
-        v2.comidaVegana();                                                          //IMPRIME LISTA DE COMIDA VEGANA QUE VENDE ESE VENDEDOR
-
+        System.out.println(); 
+        
+        v2.mostrarProductos();                              //IMPRIME LISTA TOTAL QUE VENDE ESE VENDEDOR
+        v2.bebidaSinAlcohol();                             //IMPRIME LISTA DE BEBIDAS SIN ALCOHOL QUE VENDE ESE VENDEDOR
+        v2.comidaVegana();                                //IMPRIME LISTA DE COMIDA VEGANA QUE VENDE ESE VENDEDOR
+        System.out.println("\n");
         //INSTANCIA 3
         ItemPedido p1 = new ItemPedido(plato1);
         ItemPedido p2 = new ItemPedido(plato2);
@@ -219,31 +234,37 @@ public class TPGrupo8 {
         Pedido ped1 = new Pedido("1",c1, ipm1,v1);
         Pedido ped2 = new Pedido("2",c2, ipm2,v2);
         Pedido ped3 = new Pedido("3",c3, ipm4,v4);
+        System.out.println("BUSCAR POR RANGO DE PRECIOS ---");
         ipm1.busquedaPorRangodePecios(0, 700);
         //v2.mostrarProductos(); // EJEMPLO, ESTE VENDEDOR TIENE MUCHOS ITEMS A SU VENTA
+        System.out.println("ORDENAR POR CRITERIO ---");
         ipm2.ordenarPorCriterio(); // ORDENA ALFABETICAMENTE
          //en el argumento se coloca el tipo de plato (Comida, Bebida, Comida Vegetariana y bebidas Alcoholicas), y filtra por aquellas que cumplen la condicion
+        System.out.println("FILTRAR ---");
         ipm2.filtrado('C');  // filtra por COMIDAS
         ipm2.filtrado('A'); //filtra por BEBIDAS ALCOHOLICAS
         ipm2.filtrado('B'); //filtra por BEBIDAS
         ipm2.filtrado('V'); //filtra por COMIDA VEGANA
+        System.out.println("BUSCAR POR RESTAURANTE ---");
         ipm2.buscarPorRestaurante(v2); // BUSCA QUE VENDEDOR, CUMPLE CON TODOS LOS ITEMS A PROVEER , ESTOS ITEMS SON LOS QUE ESTAN DENTRO DE DE LA INSTANCIA DE ItemsPedidosMemory
 
         
         // VERIFICACION PARA MP
         //Verifica si el precio acumulado es el correcto----OK!
+        System.out.println("\n");
         System.out.println("El costo total del pedido1 es: $"+ipm2.calcularTotal()); 
         //Compruebo el monto tatal a pagay y si el estado cambia -----OK!
         ped2.estadoDelPedido();
-        System.err.println("Pagar con MP es : $"+ped2.pagarConMP("JoaquinSola.MP"));
+        System.err.println("Costo al pagar con MP: $"+ped2.pagarConMP("JoaquinSola.MP"));
         ped2.estadoDelPedido();
 
         // VERIFICACION PARA TRANSFERENCIA
         //Verifica si el precio acumulado es el correcto----OK!
+        System.out.println("\n");
         System.out.println("El costo total del pedido2 es: $"+ipm1.calcularTotal()); 
         //Compruebo el monto tatal a pagay y si el estado cambia -----OK!
         ped1.estadoDelPedido();
-        System.err.println("Pagar con Tranferencia es es : $"+ped1.pagarConTransferencia("123456","20202020"));
+        System.err.println("Costo al pegar con TRANSFERENCIA: $"+ped1.pagarConTransferencia("123456","20202020"));
         ped1.estadoDelPedido();
         
 
